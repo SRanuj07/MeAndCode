7318af5e4a7007f85beeeb8171006db267fd2c24
Anuj Kumar Soni <anujkrsoni2001@gmail.com>
2025-09-09 14:37:53 +0000

    Changes

--- JSHitesh/keywords.js
+++ JSHitesh/keywords.js
@@ -3,3 +3,10 @@ let accountEmail = "anujkrsoni123@gmail.com";
 let accountPossword = "Anuj@12345";
 var accountCity = "Bangalore";
 console.log(accountID);
+
+// accountID = 1234567890; // Error: Assignment to constant variable.
+// console.log(accountID);
+accountEmail = "anujkrsoni1234@gmail.com"
+console.log(accountEmail);
+accountCity = "Mumbai"
+console.log(accountCity);
